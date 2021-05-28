f0add17618b1ef188ae7e486455d2259d606066f
Giovanni Pires <gpires@ebay.com>
2021-05-28 19:50:28 -0300

    Sample asserts

---
+++ test/asserts.js
@@ -0,0 +1,13 @@
+var assert = require('chai').assert
+  , foo = 'bar'
+  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+describe('Asserts', function() {
+  it("should evaluate type, equality and lengths", function() {
+    assert.typeOf(foo, 'string'); // without optional message
+    assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
+    assert.equal(foo, 'bar', 'foo equal `bar`');
+    assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
+    assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
+  });
+});
