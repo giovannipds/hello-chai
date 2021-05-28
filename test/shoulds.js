ab779740855cc24c05946c713d8b57374b40cd61
Giovanni Pires <gpires@ebay.com>
2021-05-28 20:07:48 -0300

    Shoulds

---
+++ test/shoulds.js
@@ -0,0 +1,12 @@
+var should = require('chai').should() //actually call the function
+  , foo = 'bar'
+  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+describe('Shoulds', function() {
+  it("should evaluate type, equality and lengths", function() {
+    foo.should.be.a('string');
+    foo.should.equal('bar');
+    foo.should.have.lengthOf(3);
+    beverages.should.have.property('tea').with.lengthOf(3);
+  });
+});
