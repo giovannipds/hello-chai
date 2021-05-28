6bd60edb77a14298835064b9984e0e3b6728921b
Giovanni Pires <gpires@ebay.com>
2021-05-28 20:02:16 -0300

    Testing with optional messages

--- test/expects.js
+++ test/expects.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect
   , foo = 'bar'
   , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+var answer = 43;
 
 describe('Expects', function() {
   it("should evaluate type, equality and lengths", function() {
@@ -8,5 +9,13 @@ describe('Expects', function() {
     expect(foo).to.equal('bar');
     expect(foo).to.have.lengthOf(3);
     expect(beverages).to.have.property('tea').with.lengthOf(3);
+  });
+  it("should fail", function() {
+    // AssertionError: expected 43 to equal 42.
+    expect(answer).to.equal(42);
+  });
+  it("should fail with optional message", function() {
+    // AssertionError: topic [answer]: expected 43 to equal 42.
+    expect(answer, 'topic [answer]').to.equal(42);
   });
 });
